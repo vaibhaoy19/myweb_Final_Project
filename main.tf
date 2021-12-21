2cafa11abc7a26e2d72e00a9deb89add34f17f74
lakshmiprasad2019 <58114347+lakshmiprasad2019@users.noreply.github.com>
2021-12-21 23:17:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 resource "aws_instance" "myec2" {
   instance_type          = "t2.micro"
-  ami                    = "ami-052cef05d01020f1d"
+  ami                    = "ami-0ed9277fb7eb570c9"
   
   tags = {
     Name = "import"
