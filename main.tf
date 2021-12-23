8050810a1a4c7d15890af8956cd3b2abca48aaca
lakshmiprasad2019 <58114347+lakshmiprasad2019@users.noreply.github.com>
2021-12-23 18:18:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,9 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "credentials('AWS_ACCESS_KEY_ID')"
-  secret_key = "credentials('AWS_SECRET_ACCESS_KEY')"
+  assume_role {
+    role_arn  =  var.assume-role
+  }
 }
 resource "aws_instance" "myec2" {
   instance_type          = "t2.micro"
