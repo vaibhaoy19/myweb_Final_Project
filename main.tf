54035fc4c16dbf4bbcff9db3a6b70902de18babd
lakshmiprasad2019 <58114347+lakshmiprasad2019@users.noreply.github.com>
2021-12-24 11:27:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,11 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
-  profile  = "Sample"
+    profile  = var.region
 }
 resource "aws_instance" "myec2" {
   instance_type          = "t2.micro"
-  ami                    = "ami-0ed9277fb7eb570c9"
+  ami                    = var.ami_id
   
   tags = {
     Name = "import"
