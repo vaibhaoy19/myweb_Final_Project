aee96520277e05b4f6de1099f6d3d8df2ce0a206
lakshmiprasad2019 <58114347+lakshmiprasad2019@users.noreply.github.com>
2021-12-24 11:29:27 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,3 +1,11 @@
-variable "assume-role" {
-  default = "arn:aws:iam::413509265441:role/Functional"
+variable "region" {
+  type = string
 }
+variable "ami_id" {
+  type = map(string)
+
+  default = {
+    us-east-1    = "ami-0ed9277fb7eb570c9"
+    us-west-1    = "ami-03af6a70ccd8cb578"
+    ca-central-1 = "ami-0bae7412735610274"
+  }
