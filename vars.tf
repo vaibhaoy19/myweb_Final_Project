f04f3bfd7ede0084024f797b3cc984921c8808fe
lakshmiprasad2019 <58114347+lakshmiprasad2019@users.noreply.github.com>
2022-09-12 09:35:21 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -4,12 +4,3 @@ variable "profile" {
 variable "region" {
   type = string
 }
-variable "ami_id" {
-  type = map
-
-  default = {
-    us-east-1    = "ami-0ed9277fb7eb570c9"
-    us-west-1    = "ami-03af6a70ccd8cb578"
-    ca-central-1 = "ami-0bae7412735610274"
-  }
-}
