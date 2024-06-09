82f7593475da3b815e808cbee76cb377b0cb1ee4
lakshmiprasad2019 <58114347+lakshmiprasad2019@users.noreply.github.com>
2024-06-09 09:39:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
     
 }
 resource "aws_instance" "myec2" {
-  instance_type          = "t2.medium"
+  instance_type          = "t2.micro"
   ami                    = data.aws_ami.app_ami.id
   
   
